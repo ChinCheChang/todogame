4cea45a495d283211fe2460c67f7aa5615227ceb
ChinCheChang <36709019+ChinCheChang@users.noreply.github.com>
2019-01-14 16:38:56 +0800

    add styled-components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  padding: 0.75rem 1rem;
+  border-bottom: 1px solid black;
+  width: 100%;
+  z-index: 1;
+  position: relative;
+  background-color: #e6e6e6;
+  z-index: 3;
+  a {
+    color: black;
+    &:hover {
+      font-weight: bold;
+    }
+  }
+  h1 {
+    font-weight: normal;
+    font-size: 1rem;
+    display: inline;
+  }
+`;
 
 class App extends Component {
   render() {
